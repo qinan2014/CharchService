a35061f8fdff54027d08e63de5ff16f92ad9456b
qinan@bboqi.com <478396100@qq.com>
2017-11-15 22:18:29 +0800

    编译通过

--- ChurchApp/app/src/main/cpp/native-lib.cpp
+++ ChurchApp/app/src/main/cpp/native-lib.cpp
@@ -6,6 +6,6 @@ JNIEXPORT jstring JNICALL
 Java_service_church_churchapp_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from C++ hello";
     return env->NewStringUTF(hello.c_str());
 }
