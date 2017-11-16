89519f87736f5e5d87767e4d4e149d0277c554ae
qinan@bboqi.com <478396100@qq.com>
2017-11-16 21:56:37 +0800

    修改包名路径

--- ChurchApp/app/src/main/cpp/native-lib.cpp
+++ ChurchApp/app/src/main/cpp/native-lib.cpp
@@ -3,7 +3,7 @@
 
 extern "C"
 JNIEXPORT jstring JNICALL
-Java_service_church_churchapp_MainActivity_stringFromJNI(
+Java_service_churchapp_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++ hello";
--- ChurchApp/app/src/main/java/service/churchapp/MainActivity.java
+++ ChurchApp/app/src/main/java/service/churchapp/MainActivity.java
@@ -1,4 +1,4 @@
-package service.church.churchapp;
+package service.churchapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
